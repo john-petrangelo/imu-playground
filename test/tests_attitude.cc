3527947f2dfd7079609babe9f40330f8227d32f8
John Petrangelo <>
2019-04-20 23:14:11 -0400

    Refactor to use gtest parameterized test data

--- test/tests_attitude.cc
+++ test/tests_attitude.cc
@@ -2,43 +2,30 @@
 
 #include "common.h"
 
-float const MAX_DELTA = 0.01;
-
-TEST(AttitudeTest, flat_N) {
- Vector accel(0.0, 0.0,  1.0);
- Vector mag  (0.0, 1.0, -0.5);
-
- Vector exp_euler(0.0, 1.0, 0.0);
- Vector exp_ihat(1.0, 0.0, 0.0);
- Vector exp_jhat(0.0, 1.0, 0.0);
- Vector exp_khat(0.0, 0.0, 1.0);
-
- Attitude attitude = get_attitude_from_accel_mag(accel, mag);
- EXPECT_EQ(exp_euler, attitude.euler);
- EXPECT_EQ(exp_ihat, attitude.ihat);
- EXPECT_EQ(exp_jhat, attitude.jhat);
- EXPECT_EQ(exp_khat, attitude.khat);
-}
-
 struct AttitudeTestData {
+	std::string name;
   Vector inAccel;
   Vector inMag;
   Attitude out;
 };
 
+std::ostream& operator<<(std::ostream& os, AttitudeTestData const &data)
+{
+  return os << data.name;  
+}
 
-TEST(AttitudeTest, flat_N2) {
- // { [IN] ax, ay, az, mx, my, mz, [OUT] ix, iy, iz, jx, jy, jz, kx, ky, kz}
-  AttitudeTestData flat_data[] = {
+AttitudeTestData const flat_data[] = {
     {
-      Vector(0, 0, 1), Vector(0.00, 1.00, -0.5),
+    	"Flat, 0˚ (N)",
+      Vector(0, 0, 1), Vector(0.00, 1.00, -0.5), // accel, magnetometer
       {
-        Vector(0, 0, 0),
-        Vector(1.00, 0.00, 0), Vector(0.00, 1.00, 0), Vector(0, 0, 1),
-        0, 0, 0,
+        Vector(0, 0, 0), // Euler
+        Vector(1.00, 0.00, 0), Vector(0.00, 1.00, 0), Vector(0, 0, 1), // ihat, jhat, khat
+        0, 0, 0, // heading, pitch, roll
       }
     },
     {
+    	"Flat, 30˚ (NNE)",
       Vector(0, 0, 1), Vector(0.50, 0.87, -0.5),
       {
         Vector(0, 0, 0),
@@ -47,6 +34,7 @@ TEST(AttitudeTest, flat_N2) {
       }
     },
     {
+    	"Flat, 60˚ (ENE)",
       Vector(0, 0, 1), Vector(0.87, 0.50, -0.5),
       {
         Vector(0, 0, 0),
@@ -65,19 +53,26 @@ TEST(AttitudeTest, flat_N2) {
 // 0 0 1 -0.87 0.50 -0.5 0.50 -0.87 0 0.87 0.50 0 0 0 1
 // 0 0 1 -0.50 0.87 -0.5 0.87 -0.50 0 0.50 0.87 0 0 0 1
 
-  for (int i = 0; i < 3; ++i) {
-    Vector const &inAccel =  flat_data[i].inAccel;
-    Vector const &inMag =  flat_data[i].inMag;
+class AttitudeTest : public :: testing::TestWithParam<AttitudeTestData> { };
 
-    Vector const &exp_euler(flat_data[i].out.euler);
-    Vector const &exp_ihat(flat_data[i].out.ihat);
-    Vector const &exp_jhat(flat_data[i].out.jhat);
-    Vector const &exp_khat(flat_data[i].out.khat);
+INSTANTIATE_TEST_SUITE_P(PitchZeroTests, AttitudeTest,
+	::testing::ValuesIn(flat_data));
 
-    Attitude attitude = get_attitude_from_accel_mag(inAccel, inMag);
-    EXPECT_EQ(exp_euler, attitude.euler);
-    EXPECT_EQ(exp_ihat, attitude.ihat);
-    EXPECT_EQ(exp_jhat, attitude.jhat);
-    EXPECT_EQ(exp_khat, attitude.khat);
-  }
+TEST_P(AttitudeTest, getAttitudeFromAM)
+{
+  AttitudeTestData const &data = GetParam();
+
+  Vector const &inAccel =  data.inAccel;
+  Vector const &inMag =  data.inMag;
+
+  Vector const &exp_euler(data.out.euler);
+  Vector const &exp_ihat(data.out.ihat);
+  Vector const &exp_jhat(data.out.jhat);
+  Vector const &exp_khat(data.out.khat);
+
+  Attitude attitude = get_attitude_from_accel_mag(inAccel, inMag);
+  EXPECT_EQ(exp_euler, attitude.euler);
+  EXPECT_EQ(exp_ihat, attitude.ihat);
+  EXPECT_EQ(exp_jhat, attitude.jhat);
+  EXPECT_EQ(exp_khat, attitude.khat);
 }
