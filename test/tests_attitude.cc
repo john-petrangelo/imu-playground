1872f6f75100f7cd90fcb2c9a451213746dc0fd5
John Petrangelo <>
2019-05-12 23:27:14 -0400

    Generate test data dynamically

--- test/tests_attitude.cc
+++ test/tests_attitude.cc
@@ -15,221 +15,6 @@ std::ostream& operator<<(std::ostream& os, AttitudeTestData const &data)
   return os << data.name;
 }
 
-// Sweep heading 0-330 in 30˚ steps, no pitch, no roll.
-AttitudeTestData const sweep_heading_data[] = {
-  {
-    "Flat, 0˚ (N)",
-    Vector(0, 0, -1), Vector(0, 0.5, -0.87), // accel, magnetometer
-    {
-      Vector(0, 1, 0), // Euler
-      Vector(1, 0, 0), Vector(0, 1, 0), Vector(0, 0, 1), // ihat, jhat, khat
-      0, 0, 0, // heading, pitch, roll
-    }
-  },
-  {
-    "Flat, 30˚ (NNE)",
-    Vector(0, 0, -1), Vector(-0.25, 0.43, -0.87),
-    {
-      Vector(0.5, 0.87, 0),
-      Vector(0.87, 0.5, 0), Vector(-0.5, 0.87, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "Flat, 60˚ (ENE)",
-    Vector(0, 0, -1), Vector(-0.43, 0.25, -0.87),
-    {
-      Vector(0.87, 0.5, 0),
-      Vector(0.5, 0.87, 0), Vector(-0.87, 0.5, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "Flat, 90˚ (E)",
-    Vector(0, 0, -1), Vector(-0.5, 0, -0.87),
-    {
-      Vector(1, 0, 0),
-      Vector(0, 1, 0), Vector(-1, 0, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "Flat, 120˚ (ESE)",
-    Vector(0, 0, -1), Vector(-0.43, -0.25, -0.87),
-    {
-      Vector(0.87, -0.5, 0),
-      Vector(-0.5, 0.87, 0), Vector(-0.87, -0.5, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "Flat, 150˚ (SSE)",
-    Vector(0, 0, -1), Vector(-0.25, -0.43, -0.87),
-    {
-      Vector(0.5, -0.87, 0),
-      Vector(-0.87, 0.5, 0), Vector(-0.5, -0.87, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "Flat, 180 (S)",
-    Vector(0, 0, -1), Vector(0, -0.5, -0.87),
-    {
-      Vector(0, -1, 0),
-      Vector(-1, 0, 0), Vector(0, -1, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "Flat, 210 (SSW)",
-    Vector(0, 0, -1), Vector(0.25, -0.43, -0.87),
-    {
-      Vector(-0.5, -0.87, 0),
-      Vector(-0.87, -0.5, 0), Vector(0.5, -0.87, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "Flat, 240 (WSW)",
-    Vector(0, 0, -1), Vector(0.43, -0.25, -0.87),
-    {
-      Vector(-0.87, -0.5, 0),
-      Vector(-0.5, -0.87, 0), Vector(0.87, -0.5, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "Flat, 270 (W)",
-    Vector(0, 0, -1), Vector(0.5, 0, -0.87),
-    {
-      Vector(-1, 0, 0),
-      Vector(0, -1, 0), Vector(1, 0, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "Flat, 300 (WNW)",
-    Vector(0, 0, -1), Vector(0.43, 0.25, -0.87),
-    {
-      Vector(-0.87, 0.5, 0),
-      Vector(0.5, -0.87, 0), Vector(0.87, 0.5, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "Flat, 330 (NNW)",
-    Vector(0, 0, -1), Vector(0.25, 0.43, -0.87),
-    {
-      Vector(-0.5, 0.87, 0),
-      Vector(0.87, -0.5, 0), Vector(0.5, 0.87, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-};
-
-// Heading 30˚, sweep pitch 0-90˚ in 30˚ steps, no roll.
-AttitudeTestData const sweep_pitch_data[] = {
-  {
-    "SweepPitch, -90˚",
-    Vector(0, 1, 0), Vector(-0.43, 0.75, 0.50), // accel, magnetometer
-    {
-      Vector(0, 0, -1), // Euler
-      Vector(0.87, 0, 0.5), Vector(-0.5, 0, 0.87), Vector(0, -1, 0), // ihat, jhat, khat
-      0, 0, 0, // heading, pitch, roll
-    }
-  },
-  {
-    "SweepPitch, -60˚",
-    Vector(0, 0.87, -0.5), Vector(-0.5, 0.87, 0),
-    {
-      Vector(0.5, 0.87, 0),
-      Vector(0.87, 0.5, 0), Vector(-0.5, 0.87, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "SweepPitch, -30˚",
-    Vector(0, 0, -1), Vector(-0.43, 0.75, -0.5),
-    {
-      Vector(0.87, 0.5, 0),
-      Vector(0.5, 0.87, 0), Vector(-0.87, 0.5, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "SweepPitch,   0˚",
-    Vector(0, 0, -1), Vector(-0.25, 0.43, -0.87), // accel, magnetometer
-    {
-      Vector(0.5, 0.87, 0), // Euler
-      Vector(0.87, 0.5, 0), Vector(-0.5, 0.87, 0), Vector(0, 0, 1), // ihat, jhat, khat
-      0, 0, 0, // heading, pitch, roll
-    }
-  },
-  {
-    "SweepPitch,  30˚",
-    Vector(0, 0, -1), Vector(0, 0, -1),
-    {
-      Vector(0.5, 0.87, 0),
-      Vector(0.87, 0.5, 0), Vector(-0.5, 0.87, 0), Vector(0, 0, 1),
-      0, 0, 0,
-    }
-  },
-  {
-    "SweepPitch,  60˚",
-    Vector(0, -0.87, -0.5), Vector(-0.5, -0.43, -0.75),
-    {
-      Vector(-0.25, 0.43, 0.87),
-      Vector(0.87, -0.25, -0.43), Vector(0.5, 0.43, -0.75), Vector(0, 0.87, 0.5),
-      30, 60, 0,
-    }
-  },
-  {
-    "SweepPitch,  60, python˚",
-    Vector(0, -0.87, -0.5), Vector(-0.75, 0.65, -0.13),
-    {
-      Vector(-0.25, 0.43, 0.87),
-      Vector(0.87, -0.25, -0.43), Vector(0.5, 0.43, -0.75), Vector(0, 0.87, 0.5),
-      30, 60, 0,
-    }
-  },
-  // {
-  //   "SweepPitch,  90˚",
-  //   Vector(0, -1, 0), Vector(0.43, -0.75, -0.5),
-  //   {
-  //     Vector(0, 0, 1),
-  //     Vector(0.87, 0, -0.5), Vector(0.5, 0, -0.87), Vector(0, 1, 0),
-  //     30, 90, 0,
-  //   }
-  // },
-};
-
-class AttitudeTest : public :: testing::TestWithParam<AttitudeTestData> { };
-// INSTANTIATE_TEST_SUITE_P(SweepHeadingTests, AttitudeTest, ::testing::ValuesIn(sweep_heading_data));
-// INSTANTIATE_TEST_SUITE_P(SweepPitchTests, AttitudeTest, ::testing::ValuesIn(sweep_pitch_data));
-
-TEST_P(AttitudeTest, getAttitudeFromAM)
-{
-  AttitudeTestData const &data = GetParam();
-
-  Vector const &inAccel =  data.inAccel;
-  Vector const &inMag =  data.inMag;
-
-  Vector const &exp_euler(data.out.euler);
-  Vector const &exp_ihat(data.out.ihat);
-  Vector const &exp_jhat(data.out.jhat);
-  Vector const &exp_khat(data.out.khat);
-
-  Attitude const actual = get_attitude_from_accel_mag(inAccel, inMag);
-  EXPECT_EQ(exp_euler, actual.euler);
-  EXPECT_EQ(exp_ihat, actual.ihat);
-  EXPECT_EQ(exp_jhat, actual.jhat);
-  EXPECT_EQ(exp_khat, actual.khat);
-
-  EXPECT_NEAR(data.out.heading, rad2deg(actual.heading), 0.2);
-  EXPECT_NEAR(data.out.pitch, rad2deg(actual.pitch), 0.2);
-  EXPECT_NEAR(data.out.roll, rad2deg(actual.roll), 0.2);
-}
-
 Vector const globalToLocal(float yaw, float pitch, float roll, Vector const &v)
 {
 	// All angles are inverted.
@@ -274,6 +59,7 @@ Vector const localToGlobal(float yaw, float pitch, float roll, Vector const &v) 
 	return Vector(x, y, z);
 }
 
+// Angles are expressed in radians.
 AttitudeTestData const generate_test_data(float yaw, float pitch, float roll) {
   // The unit vector representing the direction the stick is pointing.
   Vector stick = localToGlobal(yaw, pitch, roll, Vector(0, 1, 0));
@@ -281,19 +67,23 @@ AttitudeTestData const generate_test_data(float yaw, float pitch, float roll) {
   // The unit vector representing gravity (down).
   Vector accel = globalToLocal(yaw, pitch, roll, Vector(0, 0, -1));
 
-  // The unit vector representing compass north.
-  Vector mag = globalToLocal(yaw, pitch, roll, Vector(0, 1, 0));
+  // The unit vector representing magnetic north.
+  float const inclination = -60;
+  Vector mag = globalToLocal(yaw, pitch, roll,
+  	Vector(0, cos(inclination*M_PI/180), sin(inclination*M_PI/180)));
 
-  std::cout << "stick=" << stick << std::endl;
-  std::cout << "accel=" << accel << std::endl;
+  // std::cout << "stick=" << stick << std::endl;
+  // std::cout << "accel=" << accel << std::endl;
   std::cout << "mag=" << mag << std::endl;
 
 	AttitudeTestData data = {
-    "Noname˚",
+    "Noname",
     accel, mag, // accel, magnetometer
     {
       Vector(1, 0, 0), // Euler
-      Vector(1, 0, 0), Vector(0, 1, 0), Vector(0, 0, 1), // ihat, jhat, khat
+      globalToLocal(yaw, pitch, roll, Vector(1, 0, 0)), // ihat
+      globalToLocal(yaw, pitch, roll, Vector(0, 1, 0)), // jhat
+      globalToLocal(yaw, pitch, roll, Vector(0, 0, 1)), // khat
       yaw, pitch, roll,
     }
   };
@@ -301,30 +91,52 @@ AttitudeTestData const generate_test_data(float yaw, float pitch, float roll) {
 	return data;
 }
 
-TEST(AttitudeTest, testABC)
-{
-	float const yaw   = 30 * M_PI / 180;
-	float const pitch = 0 * M_PI / 180;
-	float const roll  =  0 * M_PI / 180;
+using ::testing::Combine;
+using ::testing::Range;
+using ::testing::Values;
 
-	AttitudeTestData data = generate_test_data(yaw, pitch, roll);
+class AttitudeTestCombos : public :: testing::TestWithParam<std::tuple<int, int, int>> { };
+INSTANTIATE_TEST_SUITE_P(AllCombos, AttitudeTestCombos, Combine(
+	// Yaw
+	// Values(0),
+	Range(-180, 181, 10),
+
+  // Pitch
+	// Values(0),
+	Range(-90, 91, 10),
+
+  // Roll
+	// Values(0)
+	Range(-180, 181, 10)
+));
+
+TEST_P(AttitudeTestCombos, testCombos)
+{
+	int yaw;
+	int pitch;
+	int roll;
+  std::tie(yaw, pitch, roll) = GetParam();
+
+	AttitudeTestData data = generate_test_data(
+		yaw * M_PI / 180,
+		pitch * M_PI / 180,
+		roll * M_PI / 180);
 
   Vector const &inAccel =  data.inAccel;
   Vector const &inMag =  data.inMag;
 
   // Vector const &exp_euler(data.out.euler);
-  // Vector const &exp_ihat(data.out.ihat);
-  // Vector const &exp_jhat(data.out.jhat);
-  // Vector const &exp_khat(data.out.khat);
+  Vector const &exp_ihat(data.out.ihat);
+  Vector const &exp_jhat(data.out.jhat);
+  Vector const &exp_khat(data.out.khat);
 
   Attitude const actual = get_attitude_from_accel_mag(inAccel, inMag);
-  //  EXPECT_EQ(exp_euler, actual.euler);
-  // EXPECT_EQ(exp_ihat, actual.ihat);
-  // EXPECT_EQ(exp_jhat, actual.jhat);
-  // EXPECT_EQ(exp_khat, actual.khat);
+  // EXPECT_EQ(exp_euler, actual.euler);
+  EXPECT_EQ(exp_ihat, actual.ihat);
+  EXPECT_EQ(exp_jhat, actual.jhat);
+  EXPECT_EQ(exp_khat, actual.khat);
 
-  EXPECT_NEAR(data.out.heading, actual.heading, 0.2);
-  EXPECT_NEAR(data.out.pitch, actual.pitch, 0.2);
-  EXPECT_NEAR(data.out.roll, actual.roll, 0.2);
+  // EXPECT_NEAR(data.out.heading, actual.heading, 0.2);
+  // EXPECT_NEAR(data.out.pitch, actual.pitch, 0.2);
+  // EXPECT_NEAR(data.out.roll, actual.roll, 0.2);
 }
-
